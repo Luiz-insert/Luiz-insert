7b9d29bf3b4836ce0bbdc169bf92e5781a926a9c
Luiz Gusttavo <lgusttavo29@gmail.com>
2022-04-15 22:37:06 -0300

    site incompleto alteraçoes em andamento

---
+++ main.js
@@ -0,0 +1,11 @@
+        let mClaroEscuro = document.querySelector('#divTodos'); 
+        
+        
+        
+        if(1 < 0) {
+            mClaroEscuro.classList.replace('main1', 'main2');
+        }
+        
+        
+        
+
