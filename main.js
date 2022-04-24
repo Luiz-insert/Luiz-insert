61fba376c0b60f0dedf06478f378a6e92b2e0f94
Luiz Gusttavo <lgusttavo29@gmail.com>
2022-04-24 12:34:54 -0300

    site reestilizado

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-alert('Site em desenvolvimento, projetos e recursos como contraste e modo claro/escuro não estão finalizados')
+lert('Site em desenvolvimento, projetos e recursos como contraste e modo claro/escuro não estão finalizados')
+
+var button_MudaCor = document.querySelector("#mudarCor");
+
+button_MudaCor.addEventListener('click', function (event) {
+
+  if (button_MudaCor.classList.contains('changeColor')) {
+    button_MudaCor.classList.remove('changeColor');
+  } else {
+    button_MudaCor.classList.add('changeColor');
+  }
+
+}, false);
 
 
 
