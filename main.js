4407f9f699b07e8b559427de790b7915758a7745
Luiz Gusttavo <lgusttavo29@gmail.com>
2022-06-12 11:06:50 -0300

    index.lock excluido

--- main.js
+++ main.js
@@ -1,4 +1,35 @@
 var alteraVersao = document.querySelector("#version").innerHTML = "v0.7_19/05/22_under development";
+//Não alterar*
+
+
+
+//remove o estilo claro e consequentemente o codigo em cascata le o 2°//
+
+    var removeStyle = document.querySelector("#estilo");
+    
+    var botaoClaroEscuro = document.querySelector(".bttLightDark");
+    botaoClaroEscuro.addEventListener('click', updateButton);
+
+function updateButton() {
+
+  if (botaoClaroEscuro.value === 'Modo Claro') 
+  {  
+    botaoClaroEscuro.value = 'Modo Escuro';
+    removeStyle.href = "/css/style2.css"    
+  } 
+  else 
+  {
+    botaoClaroEscuro.value = 'Modo Claro';
+    removeStyle.href = "/css/style.css"
+  }
+}
+
+
+
+
+
+
+
 
 
 
