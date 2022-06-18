39d00934a94600abf9a12764b2d00153a5ca5ac1
Luiz Gusttavo <lgusttavo29@gmail.com>
2022-06-18 13:21:10 -0300

    Tentativa 03

--- main.js
+++ main.js
@@ -1,35 +1,13 @@
 window.alert('Site em desenvolvimento, recursos e links podem não funcionar corretamente')
-
-
-
-
+//Alerta o cliente com uma ferramenta da janela do navegador que visualmente se altera de acordo com o mesmo
 
 
 var alteraVersao = document.querySelector("#version").innerHTML = "v0.8_17/06/22_under development";
-//Não alterar*
+//Altera a string de todos os paragrafos com #version.*
 
 
 
-//remove o estilo claro e consequentemente o codigo em cascata le o 2°//
 
-    var removeStyle = document.querySelector("#estilo");
-    
-    var botaoClaroEscuro = document.querySelector(".bttLightDark");
-    botaoClaroEscuro.addEventListener('click', updateButton);
-
-function updateButton() {
-
-  if (botaoClaroEscuro.value === 'Modo Claro') 
-  {  
-    botaoClaroEscuro.value = 'Modo Escuro';
-    removeStyle.href = "/css/style2.css"    
-  } 
-  else 
-  {
-    botaoClaroEscuro.value = 'Modo Claro';
-    removeStyle.href = "/css/style.css"
-  }
-}
 
 
 
