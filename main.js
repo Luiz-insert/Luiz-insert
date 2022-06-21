665c0ccde95a4201bb2c91ba8da60dd5bb3343c4
Luiz Gusttavo <lgusttavo29@gmail.com>
2022-06-21 18:33:24 -0300

    mediaqueries adicionados

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ window.alert('Site em desenvolvimento, recursos e links podem não funcionar cor
 //Alerta o cliente com uma ferramenta da janela do navegador que visualmente se altera de acordo com o mesmo
 
 
-var alteraVersao = document.querySelector("#version").innerHTML = "v0.8_17/06/22_under development";
+var alteraVersao = document.querySelector("#version").innerHTML = "v0.9_21/06/22_under development";
 //Altera a string de todos os paragrafos com #version.*
 
 
